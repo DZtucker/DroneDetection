323e849d5c0d1d3530a23de8449cf373b177e424
DZtucker <68160056+DZtucker@users.noreply.github.com>
2023-03-23 20:01:51 +0800

    Update rename.py

--- rename.py
+++ rename.py
@@ -1,5 +1,7 @@
 import os
 
+# 作用为将目标文件夹内的图片加上一个文件夹名字的前缀，防止在整合数据集时重名。
+
 path = r"D:\Coding\Python\Objectdetection\Objectdetection\Objectdetection\train"  # 文件夹路径
 
 for folder_name in os.listdir(path):
